61a0d99fa093f52a756a5c0ed560035f04cbbe88
Gurjeet0304 <gurjeetsahi@gmail.com>
2019-07-15 16:09:04 +1200

    my terraform working

---
+++ main.tf
@@ -0,0 +1,54 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "web_server_location" {}
+variable "web_server_rg" {}
+variable "resource_prefix"{}
+variable "web_server_address_space" {}
+variable "web_server_address_prefix" {}
+
+
+
+
+
+
+
+
+provider "azurerm" {
+    version = "1.28.0"
+    client_id = "${var.client_id}"
+    client_secret = "${var.client_secret}"
+    tenant_id = "${var.tenant_id}"
+    subscription_id = "${var.subscription_id}"
+  
+}
+
+resource "azurerm_resource_group" "web_server_rg" {
+    name = "${var.web_server_rg}"
+    location = "${var.web_server_location}"
+}
+
+resource "azurerm_virtual_network" "web_server_vnet" {
+    name = "${var.resource_prefix}-vnet"
+    location = "${var.web_server_location}"
+    resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+    address_space = ["${var.web_server_address_space}"]
+    subnet {
+        name = "${var.resource_prefix}-subnet"
+        address_prefix = "${var.web_server_address_prefix}"
+    }
+  
+}
+#resource "azurerm_subnet" "web_server_subnet" {
+    #name = "${var.resource_prefix}-subnet"
+    #resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+   # virtual_network_name = "${azurerm_virtual_network.web_server_vnet.name}"
+  #  address_prefix = "${var.web_server_address_prefix}"
+
+  
+#}
+
+
+
+
