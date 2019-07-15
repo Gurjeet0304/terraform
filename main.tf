90687d3e5fbbcb9f4f4d724270e31caa969ba8e4
Gurjeet0304 <gurjeetsahi@gmail.com>
2019-07-16 08:15:09 +1200

    added

--- main.tf
+++ main.tf
@@ -7,47 +7,81 @@ variable "web_server_rg" {}
 variable "resource_prefix"{}
 variable "web_server_address_space" {}
 variable "web_server_address_prefix" {}
-
-
-
-
-
-
-
+variable "web_server_name" {}
 
 provider "azurerm" {
-    version = "1.28.0"
-    client_id = "${var.client_id}"
-    client_secret = "${var.client_secret}"
-    tenant_id = "${var.tenant_id}"
-    subscription_id = "${var.subscription_id}"
+    version          = "1.28.0"
+    client_id        = "${var.client_id}"
+    client_secret    = "${var.client_secret}"
+    tenant_id        = "${var.tenant_id}"
+    subscription_id  = "${var.subscription_id}"
   
 }
 
 resource "azurerm_resource_group" "web_server_rg" {
-    name = "${var.web_server_rg}"
-    location = "${var.web_server_location}"
+    name             = "${var.web_server_rg}"
+    location         = "${var.web_server_location}"
 }
 
 resource "azurerm_virtual_network" "web_server_vnet" {
-    name = "${var.resource_prefix}-vnet"
-    location = "${var.web_server_location}"
+    name                = "${var.resource_prefix}-vnet"
+    location            = "${var.web_server_location}"
     resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
-    address_space = ["${var.web_server_address_space}"]
-    subnet {
-        name = "${var.resource_prefix}-subnet"
-        address_prefix = "${var.web_server_address_prefix}"
+    address_space       = ["${var.web_server_address_space}"]
+    #subnet {
+        #name = "${var.resource_prefix}-subnet"
+       # address_prefix = "${var.web_server_address_prefix}"
+    #}
+  
+}
+resource "azurerm_subnet" "web_server_subnet" {
+    name                 = "${var.resource_prefix}-subnet"
+    resource_group_name  = "${azurerm_resource_group.web_server_rg.name}"
+    virtual_network_name = "${azurerm_virtual_network.web_server_vnet.name}"
+   address_prefix        = "${var.web_server_address_prefix}"
+
+  
+}
+
+
+resource "azurerm_network_interface" "wev_server_nic" {
+    name                 = "${var.web_server_name}-nic"
+    location             = "${var.web_server_location}"
+    resource_group_name  = "${azurerm_resource_group.web_server_rg.name}"
+    ip_configuration {
+        name             = "${var.web_server_name}-ip"
+        subnet_id        = "${azurerm_subnet.web_server_subnet.id}"
+        private_ip_address_allocation = "dynamic"
     }
   
 }
-#resource "azurerm_subnet" "web_server_subnet" {
-    #name = "${var.resource_prefix}-subnet"
-    #resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
-   # virtual_network_name = "${azurerm_virtual_network.web_server_vnet.name}"
-  #  address_prefix = "${var.web_server_address_prefix}"
+
+resource "azurerm_kubernetes_cluster" "web_server_rg" {
+    name = "prod"
+    location = "${var.web_server_location}"
+    
+    resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+    dns_prefix = "prod-dns"
+
+    agent_pool_profile {
+
+        name = "default"
+        count = 1
+        vm_size = "Standard_DS2_v2"
+        os_type = "Linux"
+        os_disk_size_gb = 30
+    }
+    service_principal {
+        client_id = "${var.client_id}"
+        client_secret = "${var.client_secret}"
+    }
+    tags = {
+        Environment = "Production"
+    }
 
   
-#}
+}
+
 
 
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,6 @@
-web_server_location = "westus2"
+web_server_location = "australiaeast"
 web_server_rg = "web-rg"
 resource_prefix = "web-server"
 web_server_address_space = "10.0.0.0/22"
 web_server_address_prefix = "10.0.1.0/24"
+web_server_name = "web-01"
