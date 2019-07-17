ff91ce127034dc4ee9020ee11db25123542c5ff3
Gurjeet Admin <Gurjeet.Admin@9spokes.com>
2019-07-17 17:41:20 +1200

    updating files

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ variable "client_secret" {}
 variable "tenant_id" {}
 variable "web_server_location" {}
 variable "web_server_rg" {}
-variable "resource_prefix"{}
+variable "resource_prefix" {}
 variable "web_server_address_space" {}
 variable "web_server_address_prefix" {}
 variable "web_server_name" {}
